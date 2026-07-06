0ac451d03d09900ec32bc3847b1878b1e8b06e3c
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-06 21:38:35 +1000

    Wire field-mode enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
+import { FieldModeEnhancer } from './FieldModeEnhancer';
 import { WarehouseMapPage } from './features/warehouse/WarehouseMapPage';
 import './styles.css';
 import './fieldMode.css';
@@ -11,6 +12,7 @@ const isWarehouseMapRoute = window.location.pathname === '/warehouse-map';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <FieldModeEnhancer />
       {isWarehouseMapRoute ? <WarehouseMapPage /> : <App />}
     </BrowserRouter>
   </React.StrictMode>
