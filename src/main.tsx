0677d50b689899b5d3dcc700ccea18e190bd14ab
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-10 13:11:26 +1000

    Mount driver POD quality gate

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AccountsStatementWorkbench } from './AccountsStatementWorkbench';
 import { App } from './app/App';
+import { DriverPodQualityEnhancer } from './DriverPodQualityEnhancer';
 import { FieldModeEnhancer } from './FieldModeEnhancer';
 import { FieldOpsGuardRails } from './FieldOpsGuardRails';
 import { InventoryControlCenter } from './InventoryControlCenter';
@@ -33,6 +34,7 @@ import './warehousePickHandoff.css';
 import './fieldOpsGuardRails.css';
 import './stageAndLoadExecution.css';
 import './labelPrintBlackWhite.css';
+import './driverPodQuality.css';
 
 const isWarehouseMapRoute = window.location.pathname === '/warehouse-map';
 
@@ -42,6 +44,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <FieldModeEnhancer />
       <FieldOpsGuardRails />
       <StageAndLoadExecution />
+      <DriverPodQualityEnhancer />
       <RoleAwareDesktopNavigation />
       <OwnerCommandCenter />
       <OwnerOrderIntelligence />
