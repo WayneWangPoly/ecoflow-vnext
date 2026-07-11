edc8d7563e83e056d1388144b5eb8e131d7524f6
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-11 17:39:25 +0930

    Move driver-specific styles into the driver bundle

--- src/enhancers/DriverEnhancers.tsx
+++ src/enhancers/DriverEnhancers.tsx
@@ -5,6 +5,15 @@ import { DriverPodQualityEnhancer } from '../DriverPodQualityEnhancer';
 import { DriverReturnZoneCheckin } from '../DriverReturnZoneCheckin';
 import { FieldOpsGuardRails } from '../FieldOpsGuardRails';
 import { StageAndLoadExecution } from '../StageAndLoadExecution';
+import '../fieldOpsGuardRails.css';
+import '../stageAndLoadExecution.css';
+import '../labelPrintBlackWhite.css';
+import '../driverPodQuality.css';
+import '../deliveryOperations.css';
+import '../returnZoneOperations.css';
+import '../returnZoneCopyFix.css';
+import '../returnZoneGeofence.css';
+import '../driverDeparture.css';
 
 /** Driver-shell enhancer bundle - loaded only when the driver app mounts. */
 export default function DriverEnhancers() {
