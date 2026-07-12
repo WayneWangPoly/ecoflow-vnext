9c909d409a1f84ee7d93576ef622f6f21c4b6bd2
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-12 18:24:30 +0930

    Mount Warehouse Map typography controls

--- src/enhancers/WarehouseMapRouteModules.tsx
+++ src/enhancers/WarehouseMapRouteModules.tsx
@@ -3,12 +3,14 @@ import { WarehouseMapPutawayControl } from '../WarehouseMapPutawayControl';
 import { WarehouseMapRackEnhancer } from '../WarehouseMapRackEnhancer';
 import { WarehouseMapInteractionFix } from '../WarehouseMapInteractionFix';
 import { WarehouseRackPresentationEditor } from '../WarehouseRackPresentationEditor';
+import { WarehouseTypographyEditor } from '../WarehouseTypographyEditor';
 import '../warehouseProductisation.css';
 import '../warehouseProductisationFixes.css';
 import '../warehouseMapRackEnhancer.css';
 import '../warehouseMapDensityFix.css';
 import '../warehouseMapRowActionFix.css';
 import '../warehouseRackMetadata.css';
+import '../warehouseTypographyEditor.css';
 
 /** Route modules for the protected Warehouse Map feature. */
 export default function WarehouseMapRouteModules() {
@@ -16,6 +18,7 @@ export default function WarehouseMapRouteModules() {
     <>
       <WarehouseMapOwnerEdit />
       <WarehouseRackPresentationEditor />
+      <WarehouseTypographyEditor />
       <WarehouseMapRackEnhancer />
       <WarehouseMapInteractionFix />
       <WarehouseMapPutawayControl />
