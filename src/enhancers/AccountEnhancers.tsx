65e4af8fcd132225f087c1638f31e971fe9f7cbe
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-12 13:20:11 +0930

    Use live command centre for accounts dashboard

--- src/enhancers/AccountEnhancers.tsx
+++ src/enhancers/AccountEnhancers.tsx
@@ -1,9 +1,12 @@
 import { PriceMatrixWorkbench } from '../PriceMatrixWorkbench';
 import { DeliveryRunHistory } from '../DeliveryRunHistory';
 import { AccountsStatementWorkbench } from '../AccountsStatementWorkbench';
+import { OrderPlatformExperience } from '../OrderPlatformExperience';
+import { OwnerCommandCenter } from '../OwnerCommandCenter';
 import { OwnerOrderIntelligence } from '../OwnerOrderIntelligence';
 import { OwnerStoreIntelligence } from '../OwnerStoreIntelligence';
 import { RoleAwareDesktopNavigation } from '../RoleAwareDesktopNavigation';
+import '../orderPlatformTable.css';
 import '../ownerOrderIntelligence.css';
 import '../ownerStoreIntelligence.css';
 import '../storeStatementPressure.css';
@@ -11,17 +14,20 @@ import '../accountsStatementWorkbench.css';
 import '../priceMatrixWorkbench.css';
 import '../deliveryRunHistory.css';
 import '../roleAwareNavigation.css';
+import '../ownerCommandCenter.css';
 
 /** Accounts desktop capabilities without owner tracking, warehouse control or owner governance. */
 export default function AccountEnhancers() {
   return (
     <>
       <RoleAwareDesktopNavigation />
+      <OwnerCommandCenter />
       <OwnerOrderIntelligence />
       <PriceMatrixWorkbench />
       <OwnerStoreIntelligence />
       <AccountsStatementWorkbench />
       <DeliveryRunHistory />
+      <OrderPlatformExperience />
     </>
   );
 }
