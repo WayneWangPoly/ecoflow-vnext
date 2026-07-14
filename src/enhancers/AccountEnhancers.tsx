42d2d53de8885744160364c552518d9fe94987a1
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-14 19:50:07 +0930

    Apply the one-way commercial source boundary to accounts surfaces

--- src/enhancers/AccountEnhancers.tsx
+++ src/enhancers/AccountEnhancers.tsx
@@ -1,6 +1,6 @@
-import { PriceMatrixWorkbench } from '../PriceMatrixWorkbench';
 import { DeliveryRunHistory } from '../DeliveryRunHistory';
 import { AccountsStatementWorkbench } from '../AccountsStatementWorkbench';
+import { CommercialSourceBoundary } from '../CommercialSourceBoundary';
 import { OperationsClarityEnhancer } from '../OperationsClarityEnhancer';
 import { OwnerOrderIntelligence } from '../OwnerOrderIntelligence';
 import { OwnerStoreIntelligence } from '../OwnerStoreIntelligence';
@@ -9,18 +9,18 @@ import '../ownerOrderIntelligence.css';
 import '../ownerStoreIntelligence.css';
 import '../storeStatementPressure.css';
 import '../accountsStatementWorkbench.css';
-import '../priceMatrixWorkbench.css';
 import '../deliveryRunHistory.css';
 import '../roleAwareNavigation.css';
+import '../commercialSourceBoundary.css';
 
 /** Accounts desktop capabilities without owner tracking, warehouse control or owner governance. */
 export default function AccountEnhancers() {
   return (
     <>
       <RoleAwareDesktopNavigation />
+      <CommercialSourceBoundary />
       <OperationsClarityEnhancer />
       <OwnerOrderIntelligence />
-      <PriceMatrixWorkbench />
       <OwnerStoreIntelligence />
       <AccountsStatementWorkbench />
       <DeliveryRunHistory />
