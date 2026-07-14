055fc52c4e1ff09a2d65fb3e9d1ab23e1a72e08e
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-14 20:41:51 +0930

    Use authoritative server counts on the accounts dashboard

--- src/enhancers/AccountEnhancers.tsx
+++ src/enhancers/AccountEnhancers.tsx
@@ -1,10 +1,12 @@
 import { DeliveryRunHistory } from '../DeliveryRunHistory';
 import { AccountsStatementWorkbench } from '../AccountsStatementWorkbench';
+import { AuthoritativeDashboard } from '../AuthoritativeDashboard';
 import { CommercialSourceBoundary } from '../CommercialSourceBoundary';
 import { OperationsClarityEnhancer } from '../OperationsClarityEnhancer';
 import { OwnerOrderIntelligence } from '../OwnerOrderIntelligence';
 import { OwnerStoreIntelligence } from '../OwnerStoreIntelligence';
 import { RoleAwareDesktopNavigation } from '../RoleAwareDesktopNavigation';
+import '../authoritativeDashboard.css';
 import '../ownerOrderIntelligence.css';
 import '../ownerStoreIntelligence.css';
 import '../storeStatementPressure.css';
@@ -18,6 +20,7 @@ export default function AccountEnhancers() {
   return (
     <>
       <RoleAwareDesktopNavigation />
+      <AuthoritativeDashboard />
       <CommercialSourceBoundary />
       <OperationsClarityEnhancer />
       <OwnerOrderIntelligence />
