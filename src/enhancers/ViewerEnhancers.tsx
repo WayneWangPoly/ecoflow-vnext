d696a8b9a6fb42540581e9dd36af34b85f1359b3
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-20 15:42:50 +0930

    Use persistent viewer work tabs

--- src/enhancers/ViewerEnhancers.tsx
+++ src/enhancers/ViewerEnhancers.tsx
@@ -1,6 +1,6 @@
 import { RoleAwareDesktopNavigation } from '../RoleAwareDesktopNavigation';
 import { OperationsClarityEnhancer } from '../OperationsClarityEnhancer';
-import { IndustrialDesktopUiV2 } from './IndustrialDesktopUiV2';
+import { IndustrialDesktopWorkbench } from './IndustrialDesktopWorkbench';
 import '../roleAwareNavigation.css';
 
 /** Viewer receives the same inspection, filtering and comparison UI with existing write controls still unavailable. */
@@ -8,7 +8,7 @@ export default function ViewerEnhancers() {
   return (
     <>
       <RoleAwareDesktopNavigation />
-      <IndustrialDesktopUiV2 />
+      <IndustrialDesktopWorkbench />
       <OperationsClarityEnhancer />
     </>
   );
