093dddee60eff82e0fb321a42d798c0215ca70e6
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-25 12:18:14 +0930

    Allow drivers to capture sleeve barcodes

--- src/enhancers/DriverEnhancers.tsx
+++ src/enhancers/DriverEnhancers.tsx
@@ -4,7 +4,11 @@ import { DriverLocationTracker } from '../DriverLocationTracker';
 import { DriverReturnZoneCheckin } from '../DriverReturnZoneCheckin';
 import { FieldOpsGuardRails } from '../FieldOpsGuardRails';
 import { PickTaskOwnership } from '../PickTaskOwnership';
+import { QuickSleeveBarcodeCapture } from '../QuickSleeveBarcodeCapture';
 import { StageAndLoadExecution } from '../StageAndLoadExecution';
+import { WarehouseCameraAngleAssist } from '../WarehouseCameraAngleAssist';
+import { WarehouseCameraScanner } from '../WarehouseCameraScanner';
+import { WarehouseScannerGalleryEnhancer } from '../WarehouseScannerGalleryEnhancer';
 import { DriverOperationalInstructionEnhancer } from './DriverOperationalInstructionEnhancer';
 import '../fieldOpsGuardRails.css';
 import '../pickTaskOwnership.css';
@@ -15,6 +19,9 @@ import '../returnZoneOperations.css';
 import '../returnZoneCopyFix.css';
 import '../returnZoneGeofence.css';
 import '../driverDeparture.css';
+import '../warehouseProductisation.css';
+import '../warehouseCameraPerformance.css';
+import '../quickSleeveBarcodeCapture.css';
 
 /** Driver-shell modules - loaded only when the driver app mounts. */
 export default function DriverEnhancers() {
@@ -28,6 +35,10 @@ export default function DriverEnhancers() {
       <DriverLocationTracker />
       <DriverDepartureControl />
       <DriverOperationalInstructionEnhancer />
+      <QuickSleeveBarcodeCapture />
+      <WarehouseCameraScanner />
+      <WarehouseCameraAngleAssist />
+      <WarehouseScannerGalleryEnhancer />
     </>
   );
 }
