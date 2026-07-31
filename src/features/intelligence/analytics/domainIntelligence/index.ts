2a5acae81575b007ef3893f3867346085b924339
WayneWangPoly <waynewangpoly@gmail.com>
2026-07-31 14:02:54 +0930

    feat(intelligence): export Phase 4 domain workspace

---
+++ src/features/intelligence/analytics/domainIntelligence/index.ts
@@ -0,0 +1,26 @@
+export {
+  Phase4DomainIntelligenceWorkspace,
+  type Phase4DomainIntelligenceWorkspaceProps,
+} from './Phase4DomainIntelligenceWorkspace';
+export {
+  phase4ImplementationCoverage,
+  phase4DomainOrder,
+  phase4SurfaceCapabilities,
+  validatePhase4DomainManifest,
+  validatePhase4DomainRegistry,
+  type Phase4Breakdown,
+  type Phase4Capability,
+  type Phase4DataState,
+  type Phase4DomainId,
+  type Phase4DomainManifest,
+  type Phase4Handoff,
+  type Phase4ImplementationState,
+  type Phase4ManifestIssue,
+  type Phase4ManifestValidation,
+  type Phase4SurfaceCapability,
+  type Phase4TableDefinition,
+  type Phase4TimelineEntry,
+  type Phase4Trend,
+} from './domainIntelligenceContract';
+export { phase4DomainManifests } from './domainRegistry';
+export { inventoryDomainManifest } from './inventoryDomainManifest';
