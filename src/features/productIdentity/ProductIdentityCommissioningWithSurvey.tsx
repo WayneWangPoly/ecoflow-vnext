d41daa6f88b87b2838a93272e9b34b3c7566264b
WayneWangPoly <waynewangpoly@gmail.com>
2026-09-19 21:40:45 +1000

    ECOFLOW-328 NEXT5 P2: update ProductIdentityCommissioningWithSurvey.tsx

--- src/features/productIdentity/ProductIdentityCommissioningWithSurvey.tsx
+++ src/features/productIdentity/ProductIdentityCommissioningWithSurvey.tsx
@@ -19,7 +19,7 @@ import { CommercialWave2PlanCarrier } from './CommercialWave2PlanCarrier';
 import { ProductIdentityCommissioningWorkspace } from './ProductIdentityCommissioningWorkspace';
 
 const BatchNext2DraftOnlyCarrier = lazy(() => import('./BatchNext2DraftOnlyCarrier').then((module) => ({ default: module.BatchNext2DraftOnlyCarrier })));
-const BatchNext5DraftOnlyCarrier = lazy(() => import('./BatchNext5DraftOnlyCarrier').then((module) => ({ default: module.BatchNext5DraftOnlyCarrier })));
+const BatchNext5P2ResumeSubmitCarrier = lazy(() => import('./BatchNext5P2ResumeSubmitCarrier').then((module) => ({ default: module.BatchNext5P2ResumeSubmitCarrier })));
 const BatchNext2P3ResumePublishCarrier = lazy(() => import('./BatchNext2P3ResumePublishCarrier').then((module) => ({ default: module.BatchNext2P3ResumePublishCarrier })));
 
 type Props = {
@@ -33,7 +33,7 @@ export function ProductIdentityCommissioningWithSurvey(props: Props) {
   return (
     <>
       <Suspense fallback={null}>
-        <BatchNext5DraftOnlyCarrier
+        <BatchNext5P2ResumeSubmitCarrier
           role={props.role}
           onChanged={() => setCommissioningRevision((value) => value + 1)}
         />
